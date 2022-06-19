36abc28469191788d07c845fb7cf289b7f85bd43
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-20 01:32:51 +0200

    delete items

--- src/App.js
+++ src/App.js
@@ -5,35 +5,44 @@ function App() {
   const RefInput = useRef();
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
 
   const HandleChange = (e) => {
     setValue(e.target.value);
-    setIsEditing(!!value);
-    setTimeout(() => {
-      setIsEditing(false);
-    }, 2000);
+    
   };
 
   const HandleSubmit = (e) => {
     e.preventDefault();
     if (value) {
-      setItems([...items, value]);
+      setItems([...items, { id: new Date().getTime(), text: value }]);
     }
     setValue(null);
     RefInput.current.value = null;
   };
 
+  const removeItem = (id) => {
+     const itemsFilter= items.filter((item)=>item.id!== id)
+     setItems(itemsFilter)
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        
         <form onSubmit={HandleSubmit}>
           <input ref={RefInput} onChange={HandleChange}></input>
           <button>Add Todo </button>
         </form>
         <br />
         <ul>
-          {items.map((item, index) =>(<li key={index}>{item}</li>))}
+          {items.map((item, index) => {
+            return (
+              <div key={index}>
+                <li style={{display: 'inline', marginRight:'15px'}} >{item.text}</li>
+                <button onClick={() => removeItem(item.id)}>X</button>
+              </div>
+            );
+          })}
         </ul>
       </header>
     </div>
