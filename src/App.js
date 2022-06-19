170b347002de984e940b27a50638964456cdfd09
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-20 00:58:52 +0200

    to do list

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const RefInput=useRef()
+  const RefInput = useRef();
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([]);
-
+  const [isEditing, setIsEditing] = useState(false);
 
   const HandleChange = (e) => {
     setValue(e.target.value);
+    setIsEditing(!!value);
+    setTimeout(() => {
+      setIsEditing(false);
+    }, 2000);
   };
 
-  const HandleSubmit=(e)=>{
+  const HandleSubmit = (e) => {
     e.preventDefault();
-    if(value) {
-      setItems([...items,value]);
+    if (value) {
+      setItems([...items, value]);
     }
-     setValue(null)
-     RefInput.current.value =null
-  }
+    setValue(null);
+    RefInput.current.value = null;
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={HandleSubmit}>
-        <input ref={RefInput} onChange={HandleChange}></input>
+          <input ref={RefInput} onChange={HandleChange}></input>
           <button>Add Todo </button>
         </form>
       </header>
