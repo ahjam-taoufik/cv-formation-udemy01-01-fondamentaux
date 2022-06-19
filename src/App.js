f05110d5b85e2a0a3d3d8349110ebb2c7a5e51d9
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-19 22:18:55 +0200

    add input and event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import { useState } from 'react';
 import './App.css';
 
-const lib = {
-  React: {
-    href: 'https://reactjs.org',
-    name: 'React',
-  },
-};
+const libs = [
+  {name:'React',href: 'https://reactjs.org'},
+  {name:'Angular',href: 'https://angular.io'}
+];
 
 const Component = (props) => {
   return (
@@ -21,11 +20,35 @@ const Component = (props) => {
 };
 
 function App() {
+  const [values,setValues]=useState(libs)
+  const [value,setValue]=useState(null)
+  const HandleChange=(e)=>{
+      setValue({...value,[e.target.name]:e.target.value})
+  }
+  const HandleSubmit=(e)=>{
+       e.preventDefault()
+        setValues([...values,value])
+  }
+   const list=values.map((val)=>{return<Component key={val.name} name={val.name} href={val.href} />})
  
   return (
     <div className="App">
       <header className="App-header">
-        <Component name={lib.React.name} href={lib.React.href} />
+          {list}
+          <form onSubmit={HandleSubmit} >
+          <input 
+          placeholder='add name'
+          name='name'
+          onChange={HandleChange}
+         />
+
+         <input 
+          placeholder='add link'
+          name='href'
+          onChange={HandleChange}
+         />
+           <button >submit</button>
+          </form>
       </header>
     </div>
   );
