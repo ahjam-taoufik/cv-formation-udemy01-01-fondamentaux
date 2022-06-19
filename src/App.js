ef4a9a61fba093b17087b4737a4f6f0171e2f8ae
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-19 23:55:07 +0200

    add new exercice

--- src/App.js
+++ src/App.js
@@ -1,74 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-const libs = [
-  { name: 'React', href: 'https://reactjs.org' },
-  { name: 'Angular', href: 'https://angular.io' },
-];
 
-const Component = (props) => {
-  return (
-    <a
-      className="App-link"
-      href={props.href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn {props.name}
-    </a>
-  );
-};
-
-const Iterator = (props) => {
-  const list = props.values.map((val) => {
-    return <Component key={val.name} name={val.name} href={val.href} />;
-  });
-  return list;
-};
-
-const Form = (props) => {
-  return (
-    <>
-      <form onSubmit={props.HandleSubmit}>
-        <input
-          placeholder={` add ${props.placeholder[0]}`}
-          name={`${props.name[0]}`}
-          onChange={props.HandleChange}
-        />
-        <input
-          placeholder={` add ${props.placeholder[1]}`}
-          name={`${props.name[1]}`}
-          onChange={props.HandleChange}
-        />
-        <button>{props.button}</button>
-      </form>
-    </>
-  );
-};
 
 function App() {
-  const [values, setValues] = useState(libs);
-  const [value, setValue] = useState(null);
-  const HandleChange = (e) => {
-    setValue({ ...value, [e.target.name]: e.target.value });
-  };
-  const HandleSubmit = (e) => {
-    e.preventDefault();
-    setValues([...values, value]);
-  };
+  const [name,setName]=useState(null)
+  const [isVisible,setVisible]=useState(null)
+
+  const HandleChange=(e)=>{
+      setName(e.target.value)
+  }
+
+
+  useEffect(() => {
+      setVisible(!!name)
+      console.log('name : ',!!name);
+  
+  }, [name])
+  
 
   return (
     <div className="App">
       <header className="App-header">
-        <Iterator values={values} />
+            {!isVisible && <p>enter your text </p>}
+           <input onChange={HandleChange}></input>
 
-        <Form
-          HandleSubmit={HandleSubmit}
-          HandleChange={HandleChange}
-          placeholder={['name', 'link']}
-          name={['name', 'href']}
-          button={'add link'}
-        />
+            <h1>{name && `Learn ${name}`}</h1> 
+          
       </header>
     </div>
   );
