8dd17a25f2cd6ba74a4029302be2a15cfb114611
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-19 16:23:58 +0200

    next  performence

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 
+import { useState } from 'react';
 import './App.css';
 
 const Clock=()=>{
-  const date=new Date()
+    const [date,setDate]=useState(new Date())
   return(
+    <>
     <p> {`${date}`}</p>
+    <button onClick={()=>setDate(new Date)}>click</button>
+    </>
   )
 }
 
 
-
 function App() {
-
 
   return (
     <div className="App">
       <header className="App-header">
           <Clock/>
+          <Clock/>
       </header>
     </div>
   );
 }
-
 export default App;
