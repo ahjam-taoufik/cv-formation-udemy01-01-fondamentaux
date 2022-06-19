46d8a07eaa9defd613ad23dd7edf7e238ecfa117
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-19 16:55:04 +0200

    next  performence

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const Clock=()=>{
     const [date,setDate]=useState(new Date())
+
+useEffect(() => {
+  let time=setInterval(()=>{
+      setDate(new Date());
+  },1000)
+
+   return ()=>{
+    setInterval(time)
+   }
+  
+})
+
   return(
     <>
-    <p> {`${date}`}</p>
-    <button onClick={()=>setDate(new Date)}>click</button>
+    <p> {`${date.toLocaleTimeString()}`}</p>
     </>
   )
 }
-
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-          <Clock/>
           <Clock/>
       </header>
     </div>
