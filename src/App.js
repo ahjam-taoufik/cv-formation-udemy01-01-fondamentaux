b09ae3ae038bf5b682312bee788ca98be92e17e1
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-20 17:20:13 +0200

    some performence

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
 import { useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
 
-const initState={
-  value:null,
-  items:[]
-}
+const initState = {
+  value: null,
+  items: [],
+};
 
-const reducer=(state,action)=>{
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'onChangeInput':
+      return {
+        ...state,
+        value: action.payload,
+      };
+    case 'addTolist':
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    case 'removItem':
+      return {
+        ...state,
+        // items: action.payload,
+        items:state.items.filter((item) => item.id !== action.payload)
 
-    switch (action.type) {
-      case "onChangeInput":
-        return {
-          ...state,
-           value: action.payload
-        }
-      case "addTolist":
-        return {
-          ...state,
-           items:[...state.items, action.payload]
-        }
-      case "removItem":
-        return {
-          ...state,
-           items:action.payload
-        }
-      default:
-         return state
-    }
-}
-
+      };
+    default:
+      return state;
+  }
+};
 
 
 function App() {
   const RefInput = useRef();
-  const [state, dispatch] = useReducer(reducer, initState)
-  // const [value, setValue] = useState(null);
-  // const [items, setItems] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initState);
 
   const HandleChange = (e) => {
-    // setValue(e.target.value);
-       dispatch({type:'onChangeInput',payload: e.target.value})
-    
+    dispatch({ type: 'onChangeInput', payload: e.target.value });
   };
 
   const HandleSubmit = (e) => {
     e.preventDefault();
     if (state.value) {
-      // setItems([...items, { id: new Date().getTime(), text: state.value }]);
-       dispatch({type:"addTolist",payload:{ id: new Date().getTime(), text: state.value }})
-      }
-      // setValue(null);
-      dispatch({type:'onChangeInput',payload: null})
-      RefInput.current.value = null;
-    };
-    
-    const removeItem = (id) => {
-      const itemsFilter= state.items.filter((item)=>item.id!== id)
-      // setItems(itemsFilter)
-      dispatch({type:"removItem",payload:itemsFilter})
+      dispatch({
+        type: 'addTolist',
+        payload: { id: new Date().getTime(), text: state.value },
+      });
+    }
+    dispatch({ type: 'onChangeInput', payload: null });
+    RefInput.current.value = null;
+  };
+
+
+  const removeItem = (id) => {
+    // const itemsFilter = state.items.filter((item) => item.id !== id);
+    dispatch({ type: 'removItem', payload: id });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        
         <form onSubmit={HandleSubmit}>
           <input ref={RefInput} onChange={HandleChange}></input>
           <button>Add Todo </button>
@@ -73,7 +69,9 @@ function App() {
           {state.items.map((item, index) => {
             return (
               <div key={index}>
-                <li style={{display: 'inline', marginRight:'15px'}} >{item.text}</li>
+                <li style={{ display: 'inline', marginRight: '15px' }}>
+                  {item.text}
+                </li>
                 <button onClick={() => removeItem(item.id)}>X</button>
               </div>
             );
@@ -83,5 +81,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
